ed50150cf2ef0a4cfca672268971fec6bddeae02
limzerui <lim.zerui@gmail.com>
2025-09-07 02:36:18 +0800

    feat(task): implement toDataString() for Todo, Deadline, and Event

--- src/main/java/kobe/task/Event.java
+++ src/main/java/kobe/task/Event.java
@@ -14,4 +14,8 @@ public class Event extends Task {
     public String toString() {
         return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
+
+    public String toDataString() {
+        return "E | " + commonData() + " | " + from + " | " + to;
+    }
 }
